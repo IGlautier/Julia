7b28cdf9ec95c520d274bb67d91c81cc123ad3f0
IGlautier <ivanglautier@gmail.com>
2016-03-16 14:13:47 +0000

    matrix fourier

--- fft.jl
+++ fft.jl
@@ -1,3 +1,15 @@
 function fft(x)
-
+	N = length(x)
+	if N % 2 > 0
+		println("Input length must be a power of 2")
+	elseif N < 32
+		fourier(x)
+	else 
+		xEven = Array{Float64}(N/2)
+		xOdd = Array{Float64}(N/2)
+		for i=1:2:N
+			xOdd
+		
+		fft(x[0::2])
+		
 end
---
+++ matrixFourier.jl
@@ -0,0 +1,20 @@
+function matFourier(x)
+	N = length(x)
+	x = reshape(x, 1, 10)
+
+	M = Array{Float64}(N, N)
+	M = complex(M)
+	w = e^(2*pi*im/N)
+	f = 1/sqrt(N)
+	for i=1:N
+		for j=1:N
+			M[i, j] = f*w^(j*i)			
+		end
+	end
+	X = x * M
+	X = reshape(X, 10, 1)
+	for i=1:N
+		X[i] = sqrt(X[i].re^2 + X[i].im^2)
+	end
+	return X
+end
