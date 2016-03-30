ec23d6ee48f9e89b86271e96f71f7eadfc30029e
IGlautier <ivanglautier@gmail.com>
2016-03-30 09:26:10 +0000

    FFT working

--- fft.jl
+++ fft.jl
@@ -1,15 +1,34 @@
 function fft(x)
 	N = length(x)
-	if N % 2 > 0
+	M = Int64(N / 2)
+	if mod(N,2) > 0
 		println("Input length must be a power of 2")
 	elseif N < 32
 		fourier(x)
 	else 
-		xEven = Array{Float64}(N/2)
-		xOdd = Array{Float64}(N/2)
+		xEven = Array{Float64}(M)
+		xOdd = Array{Float64}(M)
+		j = 1
 		for i=1:2:N
-			xOdd
+			xOdd[j] = x[i+1]
+			xEven[j] = x[i]
+			j += 1
+		end
 		
-		fft(x[0::2])
-		
+		fOdd = Array{Float64}(M)
+		fOdd = complex(fOdd)
+		fEven = Array{Float64}(M)
+		fEven = complex(fEven)
+		fOdd = fft(xOdd)
+		fEven = fft(xEven)
+		X = Array{Float64}(N)
+		X = complex(X)
+		for k=1:M
+			X[k] = fEven[k] + e^(-2*pi*k*im/N) * fOdd[k]
+		end
+		for k=M+1:N
+			X[k] = fEven[k - M] + e^(-2*pi*k*im/N) * fOdd[k - M]
+		end
+		return X
+	end 
 end
--- matrixFourier.jl
+++ matrixFourier.jl
@@ -1,6 +1,6 @@
 function matFourier(x)
 	N = length(x)
-	x = reshape(x, 1, 10)
+	x = reshape(x, 1, 10000)
 
 	M = Array{Float64}(N, N)
 	M = complex(M)
@@ -12,7 +12,7 @@ function matFourier(x)
 		end
 	end
 	X = x * M
-	X = reshape(X, 10, 1)
+	X = reshape(X, 10000, 1)
 	for i=1:N
 		X[i] = sqrt(X[i].re^2 + X[i].im^2)
 	end
