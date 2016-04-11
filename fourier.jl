c260b22cd7913346d2e1a47f037076331ba7937c
Ivan Glautier <ivanglautier@gmail.com>
2016-04-11 14:29:30 +0100

    Update fourier.jl

--- fourier.jl
+++ fourier.jl
@@ -4,14 +4,6 @@ function fourier(x)
 	X = complex(X)
 	M = Array{Float64}(N, N)
 	
-	#=for k=1:N
-		X[k] = 0
-		X[k] = @parallel (+) for n=1:N
-			#X[k] += x[n] * e^(-2*pi*k*n/N*im)	
-			x[n] * e^(-2*pi*k*n/N*im)
-		end
-	end=#
-	
 	for k=1:N
 		X[k] = 0
 		for n=1:N
